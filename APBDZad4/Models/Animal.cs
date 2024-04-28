0401523f015ffbab7e8b32b80562c8f1e370d5b7
piszczel24 <s26479@pjwstk.edu.pl>
2024-04-29 01:53:25 +0200

    Małe poprawki modelu

--- APBDZad4/Models/Animal.cs
+++ APBDZad4/Models/Animal.cs
@@ -1,10 +1,21 @@
-﻿namespace APBDZad4.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBDZad4.Models;
 
 public class Animal
 {
-    public required int IdAnimal { get; set; }
+    [Key] [Required]
+    public required int IdAnimal { get; init; }
+
+    [Required]
     public required string Name { get; set; }
+
+    [Required]
     public required string Category { get; set; }
+
+    [Required]
     public required float Mass { get; set; }
+
+    [Required]
     public required string FurColor { get; set; }
 }
--- APBDZad4/Models/Visit.cs
+++ APBDZad4/Models/Visit.cs
@@ -1,10 +1,21 @@
-﻿namespace APBDZad4.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBDZad4.Models;
 
 public class Visit
 {
+    [Key]
     public required int IdVisit { get; set; }
+
+    [Required]
     public required int IdAnimal { get; set; }
+
+    [Required]
     public required DateTime Date { get; set; }
+
+    [Required]
     public required string Description { get; set; }
+
+    [Required]
     public required float Price { get; set; }
 }
