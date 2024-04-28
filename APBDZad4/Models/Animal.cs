0718f014d8ee236dec195fc61bd9b64f07881b96
piszczel24 <s26479@pjwstk.edu.pl>
2024-04-28 03:18:41 +0200

    Dodano model zwierzęcia

---
+++ APBDZad4/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace APBDZad4.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public float Mass { get; set; }
+    public string FurColor { get; set; }
+}
